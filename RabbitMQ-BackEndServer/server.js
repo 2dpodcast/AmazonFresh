815a564017f65ba6086684d779a69c6cfb180894
sourabhketkale <ketkalesourabh129@gmail.com>
2016-04-07 20:13:53 -0700

    Working model of Client and server with mongo and RabbitMQ integration

---
+++ RabbitMQ-BackEndServer/server.js
@@ -0,0 +1,60 @@
+//super simple rpc server example
+var amqp = require('amqp')
+	, util = require('util');
+
+var login = require('./services/login');
+
+var express = require('express');
+var http=require('http');
+var mongoSessionConnectURL = "mongodb://localhost:27017/sessions";
+var expressSession = require("express-session");
+var mongoStore = require("connect-mongo")(expressSession);
+var mongo= require('./services/mongo');
+
+
+var app = express();
+
+app.set('port', process.env.PORT || 4100);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+
+var cnn = amqp.createConnection({host:'127.0.0.1'});
+app.use(expressSession({
+	secret: 'cmpe273_teststring',
+	resave: false,  //don't save session if unmodified
+	saveUninitialized: false,	// don't create session until something stored
+	duration: 30 * 60 * 1000,
+	activeDuration: 5 * 60 * 1000,
+	store: new mongoStore({
+		url: mongoSessionConnectURL
+	})
+}));
+
+
+
+cnn.on('ready', function(){
+	console.log("listening on login_queue");
+	console.log('reached backend server');
+	cnn.queue('login_queue', function(q){
+		q.subscribe(function(message, headers, deliveryInfo, m){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			login.handle_request(message, function(err,res){
+
+				//return index sent
+				cnn.publish(m.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:m.correlationId
+				});
+			});
+		});
+	});
+});
